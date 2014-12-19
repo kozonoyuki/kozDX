71e00167bf7e2a627a3ed36eba64d955e5a1b2b5
kozonoyuki <yuki.guiter@gmail.com>
2014-12-19 11:57:05 +0900

    add ConstantBuffer

--- kozDX/ConstantBuffer.cpp
+++ kozDX/ConstantBuffer.cpp
@@ -5,5 +5,47 @@
 * @date   12/19に書き始め
 */
 
+#include "stdafx.h"
 #include "ConstantBuffer.h"
 
+template <typename T>
+koz::ConstantBuffer<T>::ConstantBuffer()
+{
+}
+
+template <typename T>
+koz::ConstantBuffer<T>::ConstantBuffer(CComPtr<ID3D11Device> pDevice)
+{
+	ID3D11Buffer *pConstantBuffer = nullptr;
+	ZeroMemory(&m_ConstantBufferDesc, sizeof(m_ConstantBufferDesc));
+	m_ConstantBufferDesc.Usage = D3D11_USAGE_DEFAULT;
+	m_ConstantBufferDesc.ByteWidth = sizeof(T);
+	m_ConstantBufferDesc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+	m_ConstantBufferDesc.CPUAccessFlags = 0;
+	HRESULT hr = pDevice->CreateBuffer(&m_ConstantBufferDesc, nullptr, &pConstantBuffer);
+	if (FAILED(hr))
+	{
+		MessageBox(nullptr, L"コンスタントバッファを作成できませんでした。", L"Error", MB_OK);
+	}
+	m_ConstantBuffer.Attach(pConstantBuffer);
+}
+
+template <typename T>
+koz::ConstantBuffer<T>::~ConstantBuffer()
+{
+
+}
+
+template <typename T>
+void koz::ConstantBuffer<T>::SetVertexShader(CComPtr<ID3D11DeviceContext> pDeviceContext, T * pConstantBuffer, UINT Slot)
+{
+	pDeviceContext->UpdateSubresource(m_ConstantBuffer, 0, nullptr, &pConstantBuffer, 0, 0);
+	pDeviceContext->VSSetConstantBuffers(Slot, 1, &m_ConstantBuffer);
+}
+
+template <typename T>
+void koz::ConstantBuffer<T>::SetPixelShader(CComPtr<ID3D11DeviceContext> pDeviceContext, T * pConstantBuffer, UINT Slot)
+{
+	pDeviceContext->UpdateSubresource(m_ConstantBuffer, 0, nullptr, &pConstantBuffer, 0, 0);
+	pDeviceContext->PSSetConstantBuffers(Slot, 1, &m_ConstantBuffer);
+}
--- kozDX/ConstantBuffer.h
+++ kozDX/ConstantBuffer.h
@@ -15,12 +15,51 @@ namespace koz
 	/**
 	* @brief コンスタントバッファクラス
 	*/
+	template <typename T>
 	class ConstantBuffer
 	{
 	private:
+		/**
+		* コンスタントバッファ
+		*/
 		CComPtr<ID3D11Buffer> m_ConstantBuffer;
 
-	public:
+		/**
+		* コンスタントバッファ構造体
+		*/
+		D3D11_BUFFER_DESC m_ConstantBufferDesc;
 
+		/**
+		* デフォルトコンストラクタ（使用禁止)
+		*/
+		ConstantBuffer();
+
+	public:
+		/**
+		* コンストラクタ
+		* @param pDevice デバイス
+		*/
+		ConstantBuffer(CComPtr<ID3D11Device> pDevice);
+
+		/**
+		* デストラクタ
+		*/
+		~ConstantBuffer();
+
+		/**
+		* データを頂点シェーダにセットする
+		* @param pDeviceContext デバコン
+		* @param pConstantBuffer データ
+		* @param Slot スロット番号
+		*/
+		void SetVertexShader(CComPtr<ID3D11DeviceContext> pDeviceContext, T * pConstantBuffer, UINT Slot);
+
+		/**
+		* データをピクセルシェーダにセットする
+		* @param pDeviceContext デバコン
+		* @param pConstantBuffer データ
+		* @param Slot スロット番号
+		*/
+		void SetPixelShader(CComPtr<ID3D11DeviceContext> pDeviceContext, T * pConstantBuffer, UINT Slot);
 	};
 }
