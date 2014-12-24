39ba5703d73936aa87db50ea30cea07ddca1b53c
kozonoyuki <yuki.guiter@gmail.com>
2014-12-25 07:12:24 +0900

    パイプラインクラス追加

---
+++ kozDX/Pipeline.cpp
@@ -0,0 +1,10 @@
+/**
+* @file   Pipeline.cpp
+* @brief  レンダーパイプライン
+* @author kozonoyuki
+* @date   12/25に書き始め
+*/
+
+#include "stdafx.h"
+#include "Pipeline.h"
+
---
+++ kozDX/Pipeline.h
@@ -0,0 +1,13 @@
+/**
+* @file   Pipeline.h
+* @brief  レンダーパイプライン
+* @author kozonoyuki
+* @date   12/25に書き始め
+*/
+
+#pragma once
+
+namespace koz
+{
+
+}
